69fca21082974dc656f31902d4cfb6bef99490a0
Evgeniy <spardasenpai@gmail.com>
2022-12-10 14:58:54 +0300

    Done

---
+++ Katalog.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Katalog extends Notebook {
+
+    public Katalog(int id, int ram, int ssd, String os, String color){
+        super(id,ram,ssd,os,color);
+    }
+ 
+}
